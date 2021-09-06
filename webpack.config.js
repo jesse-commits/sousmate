192e1768fa9d1592b62bba8bde7625e502a5562d
Jesse <jessehglvn@gmail.com>
2021-09-06 18:02:11 -0400

    code to extract the styles from the main.js file and with a chunkhash and also configured it to be minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 //delete all from dist so we have the freshest copy
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-//extract css from the main js file
+//extract css from the main js file, leverage in 2 places
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//minimize or compress the extracted stylesheet
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const postCSSPlugins = [
     require('postcss-simple-vars'),
@@ -14,15 +16,17 @@ const postCSSPlugins = [
     require("postcss-import")
 ]
 
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
+}
+
 //the same.. any configuration that can be shared with dev and build here
 let config = {
     entry: './app/assets/scripts/App.js',
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
-            }
+            cssConfig
         ]
     }
 }
@@ -30,6 +34,8 @@ let config = {
 
 //setup unique tasks for the dev statement
 if(currentTask == 'dev') {
+    //push onto the array 
+    cssConfig.use.unshift('style-loader')
     config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
@@ -46,6 +52,8 @@ if(currentTask == 'dev') {
 
 //setup unique tasks for the build statement
 if(currentTask == 'build') {
+    // push onto array, but we want to use loader we just downloaded
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader)
     config.output = {
         //previouslly 'bundled.js'
         filename: '[name].[chunkhash].js',
@@ -55,10 +63,13 @@ if(currentTask == 'build') {
     config.mode = "production"
     //save users data, copies over vendor js code as a separate file.
     config.optimization = {
-        splitChunks: {chunks: 'all'}
+        splitChunks: {chunks: 'all'},
+        minimize: true,
+        //... use default built in webpack minimizer, but b is our plugin
+        minimizer: [`...`, new CssMinimizerPlugin()]
     }
     //delete everything in the dist folder so we have the freshest copies.
-    config.plugins = [new CleanWebpackPlugin()]
+    config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})]
 }
 
 
