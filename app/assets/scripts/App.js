d349503f4f0cdbb013dd9986c955ecb5ec13bc04
Jesse <jessehglvn@gmail.com>
2021-10-23 22:54:29 -0400

    worked on the mobile menu button

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,3 +5,15 @@ import '../styles/styles.css'
 if(module.hot){
     module.hot.accept() 
 }
+
+let ourIcon = document.getElementById('menu-icon')
+let ourMenu = document.getElementById('menu')
+
+ourIcon.addEventListener('click', function(){
+    if(ourMenu.className!="show"){
+        ourMenu.className = ourMenu.className + "show"
+    }else {
+        ourMenu.className = ""
+    }
+})
+
