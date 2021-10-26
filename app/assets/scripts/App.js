63046c333ba5b15001e0c24fd063509c26674307
Jesse <jessehglvn@gmail.com>
2021-10-26 13:17:27 -0400

    completed the mobile menu styling for the home page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,13 +7,24 @@ if(module.hot){
 }
 
 let ourIcon = document.getElementById('menu-icon')
+let menuTop = document.getElementById('menu-icon__top')
+let menuMiddle = document.getElementById('menu-icon__middle')
+let menuBottom = document.getElementById('menu-icon__bottom')
 let ourMenu = document.getElementById('menu')
 
 ourIcon.addEventListener('click', function(){
     if(ourMenu.className!="show"){
+        ourIcon.className = "header__menu-icon header__menu-icon--fixed"
         ourMenu.className = ourMenu.className + "show"
+        menuTop.className = menuTop.className + " white"
+        menuMiddle.className = menuMiddle.className + " white"
+        menuBottom.className = menuBottom.className + " white"
     }else {
         ourMenu.className = ""
+        menuTop.classList.remove("white")
+        menuMiddle.classList.remove("white")
+        menuBottom.classList.remove("white")
+        ourIcon.classList.remove("header__menu-icon--fixed")
     }
-})
 
+})
